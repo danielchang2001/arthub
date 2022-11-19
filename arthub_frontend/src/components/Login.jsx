3bbf3190d3c7403f3b864feab31a9262969e2319
Daniel Chang <danielchang083@gmail.com>
2022-11-18 22:52:22 -0800

    Add google login button. Not functional yet

--- arthub_frontend/src/components/Login.jsx
+++ arthub_frontend/src/components/Login.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcButtingIn, FcGoogle } from 'react-icons/fc'; 
+import shareVideo from '../assets/share.mp4'
+import logo from '../assets/logowhite.png'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
 
 const Login = () => {
+  
   return (
-    <div>Login</div>
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type='video/mp4'
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className="shadow-2xl">
+            {/* <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_TOKEN}> */}
+            <GoogleLogin
+              // clientID = {process.env.GOOGLE_CLIENT_TOKEN}
+              // buttonText="Login"
+              onSuccess={credentialResponse => {
+                console.log(credentialResponse);
+              }}
+              onError={() => {
+                console.log('Login Failed');
+              }}
+            />
+            {/* </GoogleOAuthProvider> */}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- arthub_frontend/src/index.js
+++ arthub_frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.render(
-<Router>
-    <App/>
-</Router>,
-document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Router>
+            {/* clientId={process.env.GOOGLE_CLIENT_TOKEN} */}
+            <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_TOKEN}>
+                <App />
+            </GoogleOAuthProvider>
+        </Router>
+    </React.StrictMode>
+    // document.getElementById('root')
 );
